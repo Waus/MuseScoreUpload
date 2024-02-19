c35dc4fcdc5cd2b495ef89e1f730928838579406
Kamil Murański <kamil.muranski.official@gmail.com>
2024-02-19 01:30:20 +0100

    Z lokatorami niedziałające

--- src/main/java/PageObjectModel/HomePage.java
+++ src/main/java/PageObjectModel/HomePage.java
@@ -1,9 +1,13 @@
 package PageObjectModel;
 
 import com.microsoft.playwright.FileChooser;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class HomePage {
     private Page page;
@@ -11,24 +15,35 @@ public class HomePage {
     // Lokatory
     //String selectPdfInput = "#js-file-convert";
 
-    String selectPdfButton = "//button[normalize-space(.)='Select PDF']";
+    //String selectPdfButton = "//button[normalize-space(.)='Select PDF']";
+    //String selectPdfButton = "//button[@class='HFvdW Dhs0s nOTLW Cl2SE u_VDg']//span[text()='Select PDF']";
+    Locator selectPDFButton;
 
-    String saveButton = "//button[normalize-space(.)='Save]";
+    //String saveButton = "//button[normalize-space(.)='Save']";
+    Locator saveButton;
+    //String saveButton = "//button[normalize-space(.)='Save']";
+
     public HomePage(Page page) {
         this.page = page;
-    }
-
-    public void clickSaveButton() {
-        page.click(saveButton);
+        selectPDFButton = page.locator("button:has-text('Select PDF')");
+        saveButton = page.locator("button:has-text('Save')");
     }
 
     public void uploadFile(Path filePath) {
-        page.click(selectPdfButton);
-        //FileChooser fileChooser = page.waitForFileChooser(() -> page.getByText("Log in").click());
 
-        FileChooser fileChooser = page.waitForFileChooser(() -> page.locator(selectPdfButton).click());
-        fileChooser.setFiles(filePath);
+//        FileChooser fileChooser = page.waitForFileChooser(() -> page.getByRole(AriaRole.BUTTON,
+//                new Page.GetByRoleOptions().setName(
+//                        Pattern.compile("Select PDF", Pattern.CASE_INSENSITIVE))).click());
+
+        FileChooser fileChooser = page.waitForFileChooser(() -> {
+            selectPDFButton.click();
+        });
+        fileChooser.setFiles(Paths.get("test.pdf"));
 
         //page.setInputFiles(selectPdfInput, filePath);
+    }
+
+    public void clickSaveButton() {
+        saveButton.click();
     }
 }
